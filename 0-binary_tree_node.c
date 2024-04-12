704485d50eb7e80e42aa7c85f9762b632174752b
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-12 20:23:11 +0300

    add node begenning

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,13 +7,15 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t * new;
 
-	if (new_node == NULL)
+	new = malloc(sizeof(binary_tree_t));
+	if (!new) {
 		return (NULL);
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	return (new_node);
+	}
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+	return (new);
 }

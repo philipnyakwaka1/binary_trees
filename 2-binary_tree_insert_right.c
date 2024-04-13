c15ddd554756d7e586da6fcc98aef0d29bfe9ff9
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-13 07:18:21 +0300

    binary_trees

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,18 +10,22 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL || parent == NULL)
+	if (parent == NULL)
 		return (NULL);
-	new_node->n = value;
-	new_node->right = parent->right;
-	new_node->left = NULL;
-	new_node->parent = parent;
 
-	parent->right = new_node;
-	if (new_node->right)
-		new_node->right->parent = new_node;
-	return (new_node);
+	new = binary_tree_node(parent, value);
+
+	if (new == NULL)
+		return (NULL);
+	if (parent->right != NULL)
+	{
+		parent->right->parent = new;
+		new->right = parent->right;
+		parent->right = new;
+		return (new);
+	}
+	parent->right = new;
+	return (new);
 }
--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,14 +1,27 @@
 #include "binary_trees.h"
 /**
- * binary_tree_delete - deletes binary tree
- * @tree: binary tree to be deleted
+ * binary_tree_insert_right - inserts a node at the right-child of
+ * another node. If a right-child exists then it replaces it
+ * and the right-child becomes the new node's right-child
+ * @parent: node to insert right child
+ * @value: value of inserted right child
+ * Return: a pointer to the created node, or NULL on
+ * failure or if parent is NULL
  */
-
-void binary_tree_delete(binary_tree_t *tree)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if (tree == NULL)
-		return;
-	binary_tree_delete(tree->left);
-	binary_tree_delete(tree->right);
-	free(tree);
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL || parent == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->right = parent->right;
+	new_node->left = NULL;
+	new_node->parent = parent;
+
+	parent->right = new_node;
+	if (new_node->right)
+		new_node->right->parent = new_node;
+	return (new_node);
 }

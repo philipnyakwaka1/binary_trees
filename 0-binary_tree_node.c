25888a6e0ae133ae6225eca6ab636a0843b5c38b
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-13 07:03:52 +0300

    binary trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,12 +7,11 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t * new;
+	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
-	if (!new) {
+	if (!new)
 		return (NULL);
-	}
 	new->n = value;
 	new->left = NULL;
 	new->right = NULL;
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,18 +10,22 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL || parent == NULL)
+	if (parent == NULL)
 		return (NULL);
-	new_node->n = value;
-	new_node->left = parent->left;
-	new_node->right = NULL;
-	new_node->parent = parent;
 
-	parent->left = new_node;
-	if (new_node->left)
-		new_node->left->parent = new_node;
-	return (new_node);
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		parent->left->parent = new;
+		new->left = parent->left;
+		parent->left = new;
+		return (new);
+	}
+	parent->left = new;
+	return (new);
 }
